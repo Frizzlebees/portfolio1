d84cd7f83dee0b71dadc557b2f39bece8288c54d
Ryan Ngo <rayango92@gmail.com>
2018-03-27 13:01:07 -0700

    Add dimmer to each portfolio item

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,9 @@
 $(document).ready(function() {
-  $('#portfolio .one').on('click', function() {
+  $('.one, .two, .three, .four, .five, .six').on('click', function() {
     $('.projectOneModal').modal('show');
   });
-  $('#portfolio .two').on('click', function() {
-    $('.projectOneModal').modal('show');
-  });
-  $('#portfolio .three').on('click', function() {
-    $('.projectOneModal').modal('show');
-  });
-  $('#portfolio .four').on('click', function() {
-    $('.projectOneModal').modal('show');
-  });
-  $('#portfolio .five').on('click', function() {
-    $('.projectOneModal').modal('show');
-  });
-  $('#portfolio .six').on('click', function() {
-    $('.projectOneModal').modal('show');
+
+  $('.one, .two, .three, .four, .five, .six').dimmer({
+    on: 'hover'
   });
 });
