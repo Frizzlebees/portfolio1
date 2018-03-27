0d2c14274a4bf879a231ee802601d2bed5cf8272
Ryan Ngo <rayango92@gmail.com>
2018-03-26 23:59:32 -0700

    Add seamntic ui modal template

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#portfolio .one').on('click', function() {
+    // $('.projectOneModal').toggleClass('active');
+    $('.projectOneModal').modal('show');
+  });
+});
