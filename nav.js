ee18d55b58b4ba1237dc066ce5643b71aa809797
Ryan Ngo <rayango92@gmail.com>
2018-03-26 22:36:48 -0700

    Add css grid template for the portfolio section

--- nav.js
+++ nav.js
@@ -13,4 +13,40 @@ $(document).ready(function() {
   $('#navIconClose').on('click', function() {
     toggleNavView();
   });
+
+  // const isElementInViewport = function(el) {
+  //   if (typeof jQuery === "function" && el instanceof jQuery) {
+  //       el = el[0];
+  //   }
+
+  //   var rect = el.getBoundingClientRect();
+
+  //   return (
+  //       rect.top >= 0 &&
+  //       rect.left >= 0 &&
+  //       rect.bottom <= $(window).height() && 
+  //       rect.right <= $(window).width()
+  //   );
+  // };
+
+  // function onVisibilityChange(el, callback) {
+  //     var old_visible;
+  //     return function () {
+  //         var visible = isElementInViewport(el);
+  //         if (visible != old_visible) {
+  //             old_visible = visible;
+  //             if (typeof callback == 'function') {
+  //                 callback(old_visible);
+  //             }
+  //         }
+  //     }
+  // }
+
+  // let el = $('#about');
+
+  // var handler = onVisibilityChange(el, function(old_visible) {
+  //   console.log(old_visible);
+  // });
+
+  // $(window).on('DOMContentLoaded load resize scroll', handler); 
 });
