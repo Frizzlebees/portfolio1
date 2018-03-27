965ed88b2b41ed9e59add548ddae848c3854df41
Ryan Ngo <rayango92@gmail.com>
2018-03-27 11:49:44 -0700

    Fix modal css and also add placeholder header and title to modal

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $('#portfolio .one').on('click', function() {
-    // $('.projectOneModal').toggleClass('active');
+    $('.projectOneModal').modal('show');
+  });
+  $('#portfolio .two').on('click', function() {
+    $('.projectOneModal').modal('show');
+  });
+  $('#portfolio .three').on('click', function() {
+    $('.projectOneModal').modal('show');
+  });
+  $('#portfolio .four').on('click', function() {
+    $('.projectOneModal').modal('show');
+  });
+  $('#portfolio .five').on('click', function() {
+    $('.projectOneModal').modal('show');
+  });
+  $('#portfolio .six').on('click', function() {
     $('.projectOneModal').modal('show');
   });
 });
