8022f3db49cc9a2c099ac36a29db26831862cd1e
Ryan Ngo <rayango92@gmail.com>
2018-05-17 20:44:58 -0700

    Add carousel for modal, change portfolio.js to use MVC design for the carousel.  Create currentSlide and slides variable for the model, goToNextSlide and goToPreviousSlide for the handlers, and toggleSlideView for the view

--- client/portfolio.js
+++ client/portfolio.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
-  $('.project-one, .project-two, .project-three, .project-four, .project-five, .project-six').on('click', function() {
-    $('.project-one-modal').modal('show');
-  });
-
-  $('.portfolio-project').dimmer({
-    on: 'hover'
-  });
+  portfolio.view.setUpEventListeners();
 });
+
+const portfolio = {
+  carousel: {
+    currentSlide: 0,
+    slides: $('.slide')
+  },
+  handlers: {
+    goToNextSlide: () => {
+      portfolio.view.toggleSlideView();
+      portfolio.carousel.currentSlide = (portfolio.carousel.currentSlide + 1) % portfolio.carousel.slides.length;
+      portfolio.view.toggleSlideView();
+    },
+    goToPreviousSlide: () => {
+
+    }
+  },
+  view: {
+    setUpEventListeners: () => {
+      $('.project-one, .project-two, .project-three, .project-four, .project-five, .project-six').on('click', function() {
+        $('.project-one-modal').modal('show');
+      });
+
+      $('.portfolio-project').dimmer({
+        on: 'hover'
+      });
+
+      $('.angle.icon').on('mouseenter mouseleave', function(e) {
+        $(e.target).toggleClass('grey inverted');
+      });
+    },
+    toggleSlideView: () => {
+      $(portfolio.carousel.slides[portfolio.carousel.currentSlide]).toggleClass('hidden');
+    }
+  }
+};
+
