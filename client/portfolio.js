f6eab5dbb3ef01f9eb77a0bb702f3741fc63a66c
Ryan Ngo <rayango92@gmail.com>
2018-05-21 21:28:39 -0700

    Add slide indicators for carousel, touch up next slide and previous slide buttons

--- client/portfolio.js
+++ client/portfolio.js
@@ -5,20 +5,34 @@ $(document).ready(function() {
 const portfolio = {
   carousel: {
     currentSlide: 0,
-    slides: $('.slide')
+    slides: $('.slide'),
+    indicators: $('.slideshow-dot')
   },
   handlers: {
     goToNextSlide: () => {
+      let previousSlide = portfolio.carousel.currentSlide;
       portfolio.view.toggleSlideView();
       portfolio.carousel.currentSlide = (portfolio.carousel.currentSlide + 1) % portfolio.carousel.slides.length;
       portfolio.view.toggleSlideView();
+      portfolio.view.updateSlideIndicator(previousSlide, portfolio.carousel.currentSlide);
     },
     goToPreviousSlide: () => {
+      let previousSlide = portfolio.carousel.currentSlide;
       portfolio.view.toggleSlideView();
       if (portfolio.carousel.currentSlide != 0) {
         portfolio.carousel.currentSlide--;
+      } else {
+        portfolio.carousel.currentSlide = portfolio.carousel.slides.length - 1;
       }
       portfolio.view.toggleSlideView();
+      portfolio.view.updateSlideIndicator(previousSlide, portfolio.carousel.currentSlide);
+    },
+    goToSlide: (slideIndex) => {
+      let previousSlide = portfolio.carousel.currentSlide;
+      portfolio.view.toggleSlideView();
+      portfolio.carousel.currentSlide = slideIndex;
+      portfolio.view.toggleSlideView();
+      portfolio.view.updateSlideIndicator(previousSlide, portfolio.carousel.currentSlide);
     }
   },
   view: {
@@ -31,12 +45,16 @@ const portfolio = {
         on: 'hover'
       });
 
-      $('.angle.icon').on('mouseenter mouseleave', function(e) {
-        $(e.target).toggleClass('grey inverted');
+      $('.slideshow-dot').on('click', function(e) {
+        portfolio.handlers.goToSlide($(e.target).index());
       });
     },
     toggleSlideView: () => {
       $(portfolio.carousel.slides[portfolio.carousel.currentSlide]).toggleClass('hidden');
+    },
+    updateSlideIndicator: (previousSlide, currentSlide) => {
+      $(portfolio.carousel.indicators[previousSlide]).toggleClass('active');
+      $(portfolio.carousel.indicators[currentSlide]).toggleClass('active');
     }
   }
 };
